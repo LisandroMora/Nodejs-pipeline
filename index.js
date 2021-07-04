5aa2b080d77c72ffd4a10ff4677c38ba6a335009
Lisandro Mora <76189204+LisandroMora@users.noreply.github.com>
2021-07-03 23:54:16 -0400

    Pipeline con Heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Construyendo PipeLine con AWS!\n'
+  const msg = 'Construyendo PipeLine con Heroku!\n'
   res.end(msg);
 });
 
